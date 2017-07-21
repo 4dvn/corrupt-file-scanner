529aa2aaafffab537b0d5a468c7e31d3c8b49385
Vincent Davis <vincent@heteroskedastic.com>
2017-07-21 11:01:08 -0600

    no message

--- scanners.py
+++ scanners.py
@@ -3,9 +3,13 @@ from xlrd import open_workbook, XLRDError
 import os
 
 def dir_walker(directory):
+    filecount = 0
     for root, dirs, files in os.walk(directory, topdown=False):
         for name in files:
             #print(os.path.join(root, name))
+            filecount += 1
+            if filecount % 100 == 0:
+                print(filecount)
             yield os.path.join(root, name)
         # for name in dirs:
         #     print(os.path.join(root, name))
@@ -21,10 +25,10 @@ def check_excel(filename):
         try:
             open_workbook(filename)
         except XLRDError:
-            print("false: " + filename)
+            print("Exception: " + filename)
             return False
         else:
-            print("True: " + filename)
+            #print("True: " + filename)
             return True
     # else:
     #     print(os.path.splitext(filename))
