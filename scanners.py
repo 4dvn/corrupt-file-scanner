76360d7e70d7f9b1a2d22f766fe7a8b1faa9ea10
Vincent Davis <vincent@heteroskedastic.com>
2017-07-21 09:52:01 -0600

    example notebook and

--- scanners.py
+++ scanners.py
@@ -1,10 +1,30 @@
 from xlrd import open_workbook, XLRDError
 
+import os
 
-def test_book(filename):
-    try:
-        open_workbook(filename)
-    except XLRDError:
-        return False
-    else:
-        return True
+def dir_walker(directory):
+    for root, dirs, files in os.walk(directory, topdown=False):
+        for name in files:
+            #print(os.path.join(root, name))
+            yield os.path.join(root, name)
+        # for name in dirs:
+        #     print(os.path.join(root, name))
+
+
+def check_excel(filename):
+    '''
+    example
+    for f in dir_walker("/Users/vincentdavis/Downloads"):
+    check_excel(f)
+    '''
+    if os.path.splitext(filename)[-1]== ".XLSX":
+        try:
+            open_workbook(filename)
+        except XLRDError:
+            print("false: " + filename)
+            return False
+        else:
+            print("True: " + filename)
+            return True
+    # else:
+    #     print(os.path.splitext(filename))
